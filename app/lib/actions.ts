e61b2b71fccdf8f3d52baf36054b747d3414210b
amirabdolazimi <amirabdolazimi.v@gmail.com>
2025-04-10 08:06:21 +0330

    Chapter 15 has been completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auths";
+import { AuthError } from "next-auth";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export type State = {
@@ -111,4 +113,22 @@ export async function deleteInvoice(id: string) {
 
   await sql`delete from invoices where id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
